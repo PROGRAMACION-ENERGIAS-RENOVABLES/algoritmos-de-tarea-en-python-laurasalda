dab1f5ef65ae16b12c5dcf0e2d9b60b671908b93
laurasalda <saldanabarraza@gmail.com>
2026-04-20 12:24:59 -0600

    Add files via upload

---
+++ codigo1.py
@@ -0,0 +1,5 @@
+# energía  E=mc^2
+masa=float(input("ingrese la masa (kg): "))
+velocidad_luz=3e8
+energía=masa*velocidad_luz**2
+print(f"La energía equivalente es: {energía} joules")
---
+++ codigo2.py
@@ -0,0 +1,5 @@
+#Trabajo W=f*d
+fuerza=float(input("ingrese la fuerza (N): "))
+distancia=float(input("Ingrese la distancia (m): "))
+trabajo=fuerza*distancia
+print(f"El trabajo realizado es: {trabajo} jolules")
